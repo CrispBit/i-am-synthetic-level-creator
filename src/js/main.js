b6c7af1c95e546c9a4424662c6e55f147e172d0c
Matthew Tran <0e4ef622@gmail.com>
2017-02-22 15:45:19 -0600

    add text when no spritesheet is given

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,24 @@
-window.onload = function() {
+window.addEventListener("load", function() {
 
     var createnew = document.getElementById("createnew");
     var begindiv = document.getElementById("begin");
     var editor = document.getElementById("editor");
+    var view = document.getElementById("view");
+    var spritesheetInput = document.getElementById("spritesheet-input");
+    var noSpriteText = document.getElementById("add-spritesheet-text");
 
-    createnew.onclick = function() {
-        begindiv.style.display = "none";
-        editor.style.display = "flex";
-    };
+    createnew.addEventListener("click", function() {
 
-};
+        if (spritesheetInput.files.length > 0) {
+
+            begindiv.style.display = "none";
+            editor.style.display = "flex";
+            view.width = view.clientWidth;
+            view.height = view.clientHeight;
+            var ctx = view.getContext("2d");
+
+        } else {
+            noSpriteText.hidden = false;
+        }
+    });
+});
