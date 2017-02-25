bf9095eddafe109d67ec1d6b6f38d69a0c2dec44
Matthew Tran <0e4ef622@gmail.com>
2017-02-25 01:15:38 -0600

    put tiles on left

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,5 +1,7 @@
 function startEditor(left, right, view, level) {
 
+    initLeft(left, level);
+
     var ctx = view.getContext("2d");
 
     window.draw = function () {
@@ -19,6 +21,29 @@ function startEditor(left, right, view, level) {
 
     resizeView(view);
 
+}
+
+function initLeft(left, level) {
+
+    for (var y = 0; y < level.spritesheet.rows; y++) {
+
+        for (var x = 0; x < level.spritesheet.columns; x++) {
+
+            var tileDiv = document.createElement("div");
+            tileDiv.style.width = level.spritesheet.spriteWidth + "px";
+            tileDiv.style.height = level.spritesheet.spriteHeight + "px";
+            tileDiv.classList.add("tile");
+
+            tileDiv.style.backgroundImage =
+                "url(" + level.spritesheet.image.src + ")";
+            tileDiv.style.backgroundPosition =
+                -level.spritesheet.spriteWidth * x + "px " +
+                -level.spritesheet.spriteHeight * y + "px";
+
+            left.appendChild(tileDiv);
+
+        }
+    }
 }
 
 function resizeView(view) {
