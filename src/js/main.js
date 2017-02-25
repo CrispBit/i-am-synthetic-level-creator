9157b47160e61cae29df0ace713c1d91c7fe4653
Matthew Tran <0e4ef622@gmail.com>
2017-02-24 21:27:51 -0600

    add spritesheet config

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,45 @@
 window.addEventListener("load", function() {
 
-    var createnew = document.getElementById("createnew");
-    var begindiv = document.getElementById("begin");
-    var editor = document.getElementById("editor");
-    var view = document.getElementById("view");
-    var spritesheetInput = document.getElementById("spritesheet-input");
-    var noSpriteText = document.getElementById("add-spritesheet-text");
+    var continueButton = document.getElementById("continue"),
+        importButton = document.getElementById("import"),
+        okButton = document.getElementById("ok");
+        editor = document.getElementById("editor"),
+        view = document.getElementById("view");
 
-    createnew.addEventListener("click", function() {
+    continueButton.addEventListener("click", function() {
+
+        var spritesheetInput = document.getElementById("spritesheet-input");
+        var spritesheetConfig = document.getElementById("spritesheet-config");
+        var begindiv = document.getElementById("begin");
 
         if (spritesheetInput.files.length > 0) {
 
             begindiv.style.display = "none";
+            spritesheetConfig.style.display = "";
+
+        } else begindiv.classList.add("missing-field");
+    });
+
+    okButton.addEventListener("click", function() {
+        
+        var spritesheetConfig = document.getElementById("spritesheet-config"),
+            spritesheet = {
+                rows: document.getElementById("rows").value,
+                columns: document.getElementById("columns").value,
+                spriteWidth: document.getElementById("width").value,
+                spriteHeight: document.getElementById("height").value
+            };
+
+        if (spritesheet.rows && spritesheet.columns &&
+            spritesheet.spriteWidth && spritesheet.spriteHeight) {
+
+            spritesheetConfig.style.display = "none";
+
             editor.style.display = "flex";
             view.width = view.clientWidth;
             view.height = view.clientHeight;
             var ctx = view.getContext("2d");
 
-        } else {
-            noSpriteText.hidden = false;
-        }
+        } else spritesheetConfig.classList.add("missing-field");
     });
 });
